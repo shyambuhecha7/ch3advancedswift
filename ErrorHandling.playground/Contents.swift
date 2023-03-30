536025b6d1a1e9847da8e0b1cd5eaad242690605
Shyam <shyam.b@simformsolutions.com>
2023-03-30 21:04:40 +0530

    error handling

---
+++ ErrorHandling.playground/Contents.swift
@@ -0,0 +1,118 @@
+import UIKit
+
+var greeting = "Hello, playground"
+
+enum FormError : Error {
+    case notEnterUserName
+    case enterAge(age : Int)
+    case formNotSubmitted
+}
+
+//use a throw statement to throw an error.
+//throw FormError.enterAge(age: 21)
+
+//Propagating Errors Using Throwing Functions
+
+class ErrorHandling {
+    func showError() throws {
+        print("hello world")
+    }
+    func username(firstName : String) throws {
+        if(firstName != "Shyam") {
+            throw FormError.notEnterUserName
+        }else{
+            print("name is shyam")
+        }
+
+    }
+    
+}
+var numerator = 10
+var denominator = 0
+
+// try to divide a number by 0
+var result = ErrorHandling()
+// result.division(firstName: "Shyam")
+
+//do catch
+
+do {
+    try result.username(firstName: "s")
+}catch{
+    print("error : not match a username")
+}
+//
+
+struct Item {
+    var itemPrice : Int
+    var  itemCount : Int
+}
+
+enum VedingErrors : Error {
+    case error1,error2,error3
+}
+//class VedingMachine{
+//
+//    var inventory  = [
+//        "chocolate" : item(itemPrice: 50, itemCount: 2),
+//        "milk" : item(itemPrice: 30, itemCount: 0),
+//        "atta" : item(itemPrice: 20, itemCount: 3),
+//    ]
+//
+//    var balancecoins = 0
+//
+//    func vend(name : String) throws {
+//        guard let n = inventory[name] else{
+//            throw VedingErrors.error1
+//        }
+//
+//        guard n.count > 0 else {
+//            throw VedingErrors.error2
+//        }
+//        balancecoins -=  n.item
+//    }
+//}
+//
+//var objV = VedingMachine()
+//try objV.vend(name: "S")
+
+
+//
+enum DivisionError: Error {
+  
+  case dividedByZero
+}
+
+func division(n1: Int, denominator: Int) throws -> Int{
+  if denominator == 0 {
+      throw VedingErrors.error2
+  }else {
+    let result = n1 / denominator
+    return result
+  }
+}
+
+
+var a = try? division(n1: 30, denominator: 10)
+print(a)
+
+//disable error handling
+var b = try! division(n1: 30, denominator: 2)
+print(b)
+
+func deferr() {
+    defer {print("defer1 called")}
+    defer {print("defer2 called")}
+    print("function called")
+}
+deferr()
+
+func deferTry() -> Int {
+    print("function deferr called")
+    defer { print("return after defer called")}
+    return 5
+   
+}
+
+var defertry = deferTry()
+print(defertry)
